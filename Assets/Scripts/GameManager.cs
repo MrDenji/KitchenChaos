7c785124768bdc8cd7e36bb0c89aa4509f6383ce
Kamil <kuczborski.kk@gmail.com>
2023-03-30 08:34:36 +0200

    Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+
+
+
+    public event EventHandler OnStateChanged;
+
+    private enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State state;
+    private float waitingToStartTimer = 1f;
+    private float countdownToStartTimer = 3f;
+    private float gamePlayingTimer;
+    private float gamePlayingTimerMax = 10f;
+
+
+
+    private void Awake()
+    {
+        state = State.WaitingToStart;
+        Instance = this;
+    }
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if(waitingToStartTimer <= 0f)
+                {
+                    state = State.CountdownToStart;
+                    OnStateChanged?.Invoke(this, new EventArgs());
+                }
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer <= 0f)
+                {
+                    state = State.GamePlaying;
+                    gamePlayingTimer = gamePlayingTimerMax;
+                    OnStateChanged?.Invoke(this, new EventArgs());
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer <= 0f)
+                {
+                    state = State.GameOver;
+                    OnStateChanged?.Invoke(this, new EventArgs());
+                }
+                break;
+            case State.GameOver:
+                OnStateChanged?.Invoke(this, new EventArgs());
+                break;
+        }
+        Debug.Log(state);
+    }
+
+    public bool IsGamePlaying()
+    {
+        return state == State.GamePlaying;
+    }
+    public bool IsCountdownToStartActive()
+    {
+        return state == State.CountdownToStart;
+    }
+
+    public float GetCountdownToStartTimer()
+    {
+        return countdownToStartTimer;
+    }
+    public bool IsGameOver()
+    {
+        return state == State.GameOver;
+    }
+
+    public float GetGamePlayingTimerNormalized()
+    {
+        return 1-(gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+}
